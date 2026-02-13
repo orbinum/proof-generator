42f734b29b8689f0212719fbba1cb6cfa6c28a56
nol4lej <nome.ahumada@outlook.com>
2026-02-13 01:52:45 -0300

    feat: Add Jest configuration for testing setup

---
+++ jest.config.js
@@ -0,0 +1,35 @@
+/**
+ * Jest configuration for @orbinum/proof-generator tests
+ */
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],
+  testMatch: ['**/*.test.ts'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+  ],
+  coverageDirectory: 'coverage',
+  verbose: true,
+  testTimeout: 30000, // 30 seconds (proof generation can be slow)
+
+  // Support ES modules from wasm-pack
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+        tsconfig: {
+          module: 'esnext',
+        },
+      },
+    ],
+  },
+};
+
